deb17ae71a1100483d3ca039901f514b49218839
glitch-fate <ra.jarav@duocuc.cl>
2026-06-24 13:43:50 -0400

    EurekaServer

---
+++ eureka server/eureka-server/src/main/duoc/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package cl.duoc.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
